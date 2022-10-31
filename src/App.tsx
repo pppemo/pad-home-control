cd161691011f3d601e3d55bafb0c77f77b4066b1
Przemek <>
2022-10-31 20:15:10 +0100

    Light switches widgets fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,48 @@
 import React from 'react';
 import { CommunicationPanel } from './components/CommunicationPanel/CommunicationPanel';
+import { Button } from './components/Button/Button';
 import { useTranslation } from 'react-i18next';
+import { useMachine } from '@xstate/react';
+import { appMachine } from './App.machine';
+import { WidgetTile } from './components/WidgetTile/WidgetTile';
+import { LightSwitch } from './widgets/LightSwitch/LightSwitch';
 import './App.module.scss';
 
 function App() {
   const { t } = useTranslation();
+  const [state, send] = useMachine(appMachine);
+
+  const LeftButtons = (
+    <>
+      <Button color="primary" onClick={() => send('OPEN_MENU')}>
+        OPEN menu
+      </Button>
+      <Button color="primary" onClick={() => send('CLOSE_MENU')}>
+        CLOSE
+      </Button>
+    </>
+  );
 
   return (
     <>
       <CommunicationPanel
         title="Finally! Welcome!"
         neutralLabel="OK"
-        onNeutralClick={() => alert('Hej!')}
+        onNeutralClick={() => {}}
+        buttonsLeft={LeftButtons}
       >
-        This is incredibly awesome!!!
+        <div style={{ width: 250, height: 250 }}>
+          <LightSwitch label="Salon" />
+        </div>
+        <div style={{ width: 250, height: 250 }}>
+          <WidgetTile
+            isConfigOpen
+            onDeleteClick={() => {}}
+            onSettingsClick={() => {}}
+          >
+            <LightSwitch isOn label="Salon" />
+          </WidgetTile>
+        </div>
       </CommunicationPanel>
     </>
   );
--- src/widgets/LightSwitch/LightSwitch.tsx
+++ src/widgets/LightSwitch/LightSwitch.tsx
@@ -1,20 +1,34 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
 import { Textfit } from 'react-textfit';
 import cn from 'classnames';
 import styles from './LightSwitch.module.scss';
 
 interface LightSwitchProps {
-  isOn: boolean;
+  isOn?: boolean;
   label: string;
   onClick?: (event: React.MouseEvent<HTMLDivElement>) => void;
 }
 export const LightSwitch: FunctionComponent<LightSwitchProps> = ({
-  isOn,
+  isOn: isOnDefault = false,
   label,
   onClick,
 }) => {
+  const [isOn, setIsOn] = useState<boolean>(isOnDefault);
+
+  useEffect(() => {
+    setIsOn(isOnDefault);
+  }, [isOnDefault]);
+
+  const handleOnClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    setIsOn(!isOn);
+    onClick && onClick(event);
+  };
+
   return (
-    <div className={cn(styles.root, { [styles.on]: isOn })} onClick={onClick}>
+    <div
+      className={cn(styles.root, { [styles.on]: isOn })}
+      onClick={handleOnClick}
+    >
       <div className={cn(styles.background, { [styles.visible]: isOn })} />
       <Textfit className={styles.textfit}>{label}</Textfit>
     </div>
